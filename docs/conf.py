33835c42f1becfc02e161d3b52b796ed9306b69f
jgliss <jg@nilu.no>
2017-01-06 15:54:20 +0100

    Mock basemap from docs, because autodoc does not work

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
         return Mock()
 
 MOCK_MODULES = [
-    'basemap',
+    'mpl_toolkits.basemap',
 ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # If extensions (or modules to document with autodoc) are in another directory,
