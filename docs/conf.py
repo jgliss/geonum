71a5cfcd33b11bdf47c8711f549ffa360cf3b3bf
jgliss <jg@nilu.no>
2017-02-14 13:21:16 +0100

    Try resolving basemap ImportError for building docs on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ class Mock(MagicMock):
         return Mock()
 
 MOCK_MODULES = [
-    #'mpl_toolkits.basemap',
+    'mpl_toolkits.basemap',
+    'basemap'
 ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # If extensions (or modules to document with autodoc) are in another directory,
