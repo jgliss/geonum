8c1fa71ec92a413922bfc2fdc306c5867b9c847f
jgliss <jg@nilu.no>
2017-08-26 12:09:14 +0200

    Worked on docs

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ MOCK_MODULES = [
     'pyproj'
 ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
