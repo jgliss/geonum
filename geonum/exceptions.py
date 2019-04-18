54b8324ce02db154dd0b35746319ca0b12388008
jgliss <jg@nilu.no>
2019-04-18 17:03:02 +0200

    New module exceptions.py (contains exceptions previously defined in topodata.py module)

---
+++ geonum/exceptions.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Geonum is a Python library for geographical calculations in 3D
+# Copyright (C) 2017 Jonas Gliss (jonasgliss@gmail.com)
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License a
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Custom exceptions of geonum"""
+class SRTMNotCoveredError(ValueError):
+    pass
+
+class TopoAccessError(IOError):
+    pass
+
+class InvalidTopoMode(IOError):
+    pass
