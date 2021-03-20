5fe11f412f20ddf8f956206dc7115b104bc53201
jgliss <jonasg@met.no>
2021-03-20 19:49:58 +0100

    Minor reformatting in example scripts

--- scripts/ex5_topodata.py
+++ scripts/ex5_topodata.py
@@ -1,6 +1,6 @@
 """geonum example script 5
 
-Low level access to topographic data (here SRTM) using the example of 
+Low level access to topographic data (here SRTM) using the example of
 Oslo (Norway).
 
 Note
@@ -21,42 +21,41 @@ def load_topo_oslo():
     dat = geonum.TopoDataAccess(mode='srtm')
     topodata = dat.get_data(59.8728, 10.375, 60.026, 10.9144)
     return topodata
-    
-    
+
+
 if __name__ == "__main__":
     topo_data = load_topo_oslo()
     my_flat = geonum.GeoPoint(59.919386, 10.714970,
                               name="Somewhere in Frogner (Oslo)")
-    
+
     if geonum.BASEMAP_AVAILABLE:
         basemap = topo_data.plot_2d()
         basemap.ax.set_title("SRTM topo data Oslo")
         basemap.draw_mapscale_auto()
-        
-        
+
+
         my_flat.plot_2d(basemap, add_name=True)
         basemap.ax.figure.savefig(join(save_path, "ex5_out_1_oslo_map.png"))
-        
+
     # Import script options
     (options, args) = OPTPARSE.parse_args()
-    
-    # If applicable, do some tests. This is done only if TESTMODE is active: 
-    # testmode can be activated globally (see SETTINGS.py) or can also be 
-    # activated from the command line when executing the script using the 
+
+    # If applicable, do some tests. This is done only if TESTMODE is active:
+    # testmode can be activated globally (see SETTINGS.py) or can also be
+    # activated from the command line when executing the script using the
     # option --test 1
     if int(options.test):
         from os.path import basename
         npt.assert_array_equal([], [])
-        
+
         actual = [my_flat.altitude]
         npt.assert_allclose(actual=actual,
                             desired=[44.0],
                             rtol=1e-6)
-        print(("All tests passed in script: %s" %basename(__file__))) 
+        print(("All tests passed in script: %s" %basename(__file__)))
     try:
         if int(options.show) == 1:
             show()
     except:
         print("Use option --show 1 if you want the plots to be displayed")
-    
-    
+
