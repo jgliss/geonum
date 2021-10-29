4439390fe439c35a136c6105a9812db5ee0499a8
jgliss <jonasgliss@gmail.com>
2021-10-29 13:18:07 +0200

    Update intersphinx_mapping in confg.py

--- docs/conf.py
+++ docs/conf.py
@@ -319,7 +319,13 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python'    : ('https://docs.python.org/3', None),
+    'numpy'     : ('https://numpy.org/doc/stable', None),
+    'pandas'    : ('https://pandas.pydata.org/docs', None),
+    'scipy'     : ('https://docs.scipy.org/doc/scipy', None),
+    'matplotlib': ('https://matplotlib.org/stable', None)
+    }
 
 #: names of private methods that are supposed to be included in docs
 def skip(app, what, name, obj, skip, options):
