f989de63ac247135e47ac8b64e7710f818063dda
jgliss <jonasgliss@gmail.com>
2021-10-30 08:38:09 +0200

    Revise docs in LineOnGrid and apply minor updates to code

--- geonum/lineongrid.py
+++ geonum/lineongrid.py
@@ -13,9 +13,10 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.ndimage import map_coordinates
+
 
 class LineOnGrid(object):
     """A class representing a line on a discrete grid
@@ -40,50 +41,59 @@ class LineOnGrid(object):
     name : int
         string for identification (optional)
 
+    Example
+    -------
+
+    >>> import numpy as np
+    >>> from geonum import LineOnGrid
+    >>> data = np.ones((10,15)) * np.arange(15)
+    >>> line = LineOnGrid(2,2,5,5)
+    >>> profile = line.get_line_profile(data)
+    >>> print(profile)
+    [2.         2.74392912 3.50304939 4.24920976 5.        ]
+
     """
     def __init__(self, x0, y0, x1, y1, name=None):
         if name is None:
             name = 'undefined'
         self.name = name
-        self.start = [x0,y0]
-        self.stop = [x1,y1]
+        self.start = [x0, y0]
+        self.stop = [x1, y1]
 
-        self.profile_coords = None
+        self._profile_coords = None
 
-        #self.check_coordinates()
         self._det_normal_vec()
-        self.prepare_profile_coordinates()
 
-    def check_coordinates(self):
-        """Check if coordinates are in the right order and swap, if not"""
-        if any([x < 0 for x in self.start]):
-            raise ValueError("Invalid value encountered, coords must not be"
-                " smaller than 0")
-        if any([x < 0 for x in self.stop]):
-            raise ValueError("Invalid value encountered, coords must not be"
-                " smaller than 0")
-        if self.start[0] > self.stop[0]:
-            print("X coordinate of Start point is larger than X of stop point")
-            print("Start and Stop will be exchanged")
-            self.start, self.stop = self.stop, self.start
+    @property
+    def profile_coords(self) -> 'numpy.ndarray':
+        """(y,x) profile coordinates"""
+        if self._profile_coords is None:
+            self._profile_coords = self._prepare_profile_coordinates()
+        return self._profile_coords
 
-    def prepare_profile_coordinates(self):
+
+    def _prepare_profile_coordinates(self):
         """Prepare the evaluation coordinates as stack"""
         length = self.length
         x0, y0 = self.start
         x1, y1 = self.stop
-        x = np.linspace(x0, x1, length+1)
-        y = np.linspace(y0, y1, length+1)
-        self.profile_coords = np.vstack((y,x))
+        x = np.linspace(x0, x1, length + 1)
+        y = np.linspace(y0, y1, length + 1)
+        return np.vstack((y, x))
 
     @property
-    def normal_vector(self):
-        """Normal vector of line"""
+    def normal_vector(self) -> tuple:
+        """Normal vector of line (x, y)"""
         return self._det_normal_vec()
 
     @property
-    def length(self):
-        """Determine the length in grid coordinates"""
+    def length(self) -> int:
+        """Determine the length in units of grid indices
+
+        Note
+        ----
+        The length is rounded to integer precision.
+        """
         del_x, del_y = self._delx_dely()
         return int(round(np.hypot(del_x, del_y)))
 
@@ -102,7 +112,7 @@ class LineOnGrid(object):
 
         Parameters
         ----------
-        array : ndarray
+        array : numpy.ndarray
             2D data array
         **kwargs
             additional keyword args that are passed to
@@ -110,7 +120,7 @@ class LineOnGrid(object):
 
         Returns
         -------
-        ndarray
+        numpy.ndarray
             1D array containing the values along the line profile coordinates
         """
         return map_coordinates(array, self.profile_coords, **kwargs)
@@ -118,35 +128,55 @@ class LineOnGrid(object):
     def plot_line_on_grid(self, array, ax=None, **kwargs):
         """Plot this line on an input array using imshow
 
-        :param ndarray array: the data array
-        :param **kwargs: additional keyword arguments for imshow
+        Parameters
+        ----------
+        array : numpy.ndarray
+            the data array to which this line is mapped
+        ax : matplotlib.axes.Axes, optional
+            axes object
+        **kwargs
+            additional keyword arguments for imshow
+
+        Returns
+        -------
+        matplotlib.axes.Axes
+            axes instance used for plotting
 
         """
-        import matplotlib.pyplot as plt
         if ax is None:
-            fig, ax = plt.subplots(1,1)
+            fig, ax = plt.subplots(1, 1)
+        else:
+            fig = ax.figure
 
-        im = ax.imshow(array, cmap="gray", interpolation='none',
-                       origin="upper", **kwargs)
+        imshow_args = dict(cmap="gray", interpolation='none', origin="upper")
+        imshow_args.update(kwargs)
+        im = ax.imshow(array, **imshow_args)
         fig.colorbar(im, ax=ax, shrink=0.9)
-        ax.plot([self.start[0], self.stop[0]], [self.start[1], self.stop[1]],
+        ax.plot([self.start[0], self.stop[0]],
+                [self.start[1], self.stop[1]],
                 'co-')
 
         ax.set_xlim([0, array.shape[1] - 1])
         ax.set_ylim([array.shape[0] - 1, 0])
         return ax
 
-    def plot_line_profile(self, array, ax=None, **kwargs):
-        """Plot the line profile
+    def plot_line_profile(self, array, ax=None):
+        """Plot profile of line in input data array
 
-        :param ndarray array: the data array
-        :param ax (None): axes object
-        :param **kwargs: additional keyword arguments for imshow
+        Parameters
+        ----------
+        array : numpy.ndarray
+            the data array to which this line is mapped
+        ax : matplotlib.axes.Axes, optional
+            axes object
 
+        Returns
+        -------
+        matplotlib.axes.Axes
+            axes instance used for plotting
         """
         if ax is None:
-            import matplotlib.pyplot as plt
-            fig, ax = plt.subplots(1,1)
+            fig, ax = plt.subplots(1, 1)
         p = self.get_line_profile(array)
         ax.set_xlim([0, self.length])
         ax.grid()
@@ -154,30 +184,30 @@ class LineOnGrid(object):
         ax.set_title("Line profile")
         return ax
 
-    def plot(self, array, fig_width=12):
+    def plot(self, array):
         """High level plot function, calls:
 
             1. :func:`plot_line_on_grid`
-            #. :func:`plot_line_profile`
+            2. :func:`plot_line_profile`
 
         and puts them next to each other in a subplot
 
-        :param ndarray array: the data array
-        :param int fig_width: width of figure in inch
+        Parameters
+        ----------
+        array : numpy.ndarray
+            2D data array to which this line is mapped
 
+        Returns
+        -------
+        matplotlib.figure.Figure
+            figure instance containing the plots.
         """
-        import matplotlib.pyplot as plt
+        fig, axes = plt.subplots(2, 1)
 
-        dx, dy = self._delx_dely()
-        if dx > dy:
-            r = float(dy) / dx
-            h = int(fig_width * r * 2.5)
-            fig, axes = plt.subplots(2,1, figsize=(fig_width, h))
-        else:
-            fig, axes = plt.subplots(1,2, figsize = (18, 6))
         self.plot_line_on_grid(array, ax=axes[0])
         self.plot_line_profile(array, ax=axes[1])
         plt.tight_layout()
+        return fig
 
     def _delx_dely(self):
         """Returns length of x and y range"""
@@ -185,7 +215,13 @@ class LineOnGrid(object):
                 abs(self.stop[1] - self.start[1]))
 
     def _det_normal_vec(self):
-        """Determines the normal vector of the line"""
+        """Determines the normal vector (orientation) of the line
+
+        Returns
+        -------
+        tuple
+            2-element tuple containing (x, y) elements of normal vector
+        """
         delx, dely = self._delx_dely()
         try:
             a = np.arctan(float(dely) / float(delx))
@@ -193,9 +229,9 @@ class LineOnGrid(object):
             a = np.pi / 2
         return (np.sin(a), np.cos(a))
 
-    """Magic methods"""
     def __str__(self):
         """String representation"""
-        s = ("LineOnGrid %s\nStart (x,y): %s\nStop (x,y): %s\n"
-             % (self.name, self.start, self.stop))
+        s = (
+            f"LineOnGrid {self.name}. Start (x,y): {self.start}. Stop (x,"
+            f"y): {self.stop}")
         return s
