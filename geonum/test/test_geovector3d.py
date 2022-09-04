a54ab608a722e89bb7c08617c01450834126b4ec
jgliss <jonasgliss@gmail.com>
2022-09-04 15:52:55 +0200

    fix failing test

--- geonum/test/test_geovector3d.py
+++ geonum/test/test_geovector3d.py
@@ -2,5 +2,5 @@ import pytest
 import geonum.geovector3d as mod
 
 def test_GeoVector3D___init__NO_INPUT():
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         mod.GeoVector3D()
