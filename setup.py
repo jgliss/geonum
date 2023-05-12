667c25e3dff598b1d1197d0e507a1b6f03bcf625
jgliss <jonasgliss@gmail.com>
2023-05-12 16:41:30 +0200

    remove references to geonum/local_topo_data

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import os
+
 from setuptools import setup, find_packages
 
 with open('README.rst') as file:
     readme = file.read()
 
 setup(
-    name        =   'geonum',
-    version     =   os.environ.get("RELEASE_TAG", ""),
-    author      =   'Jonas Gliss',
-    author_email=   'jonasgliss@gmail.com',
-    license     =   'GPLv3',
-    url         =   'https://github.com/jgliss/geonum',
-    package_dir =   {'geonum'     :   'geonum'},
-    packages    =   find_packages(exclude=['contrib', 'docs', 'tests*']),
-    include_package_data    =   True,
-    package_data=   {'geonum'     :   ['local_topo_data/*.rst',
-                                       'local_topo_data/*.txt']},
+    name='geonum',
+    version=os.environ.get("RELEASE_TAG", ""),
+    author='Jonas Gliss',
+    author_email='jonasgliss@gmail.com',
+    license='GPLv3',
+    url='https://github.com/jgliss/geonum',
+    package_dir={'geonum': 'geonum'},
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    include_package_data=True,
+    package_data={},
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
@@ -34,7 +34,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.,
         'Programming Language :: Python :: 3'
     ],
-    description = 'Toolbox for 3D geonumerical calculations and atmospheric composition',
-    long_description = readme,
+    description='Toolbox for 3D geonumerical calculations and atmospheric composition',
+    long_description=readme,
     long_description_content_type='text/x-rst'
 )
