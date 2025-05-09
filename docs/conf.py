35843f5df0604c83028061de15474e15e728da02
Jonas Gliss <jonasgliss@gmail.com>
2025-05-09 11:20:13 +0200

    remove print statement in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,6 @@ _version = setuptools_scm.get_version("..", relative_to=__file__)
 if _version == "0.0.0":
     raise ValueError("something is off retrieving the correct geonum version using setuptools_scm.get_version")
 
-print("BLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 print(_version)
 
 from unittest.mock import MagicMock as Mock
